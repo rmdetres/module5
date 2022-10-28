21308fd6178a1983b7f01730aaccf3ff0b099b1b
Ryan Detres <rmdetres@gmail.com>
2022-10-28 14:50:12 -0500

    new code

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 // save reference to important DOM elements
 var timeDisplayEl = $('#time-display');
-var firstNameEl = $('input[name="first-name"]');
-var alertButton9am = document.getElementById("#9amBtn");
-var nineAmTxt = document.getElementById("#9amTxt");
+var alertButtonNineAm =  document.getElementById("9amBtn");
+var nineAmTxt = document.getElementById("9amTxt");
+var nineAm;
 
 
 // handle displaying the time
-
-
 function displayTime() {
   var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
   timeDisplayEl.text(rightNow);
@@ -16,18 +14,33 @@ setInterval(displayTime, 1000);
 
 
 
-// handle saving from alert buttons
-  var nineAM = localStorage.getItem("nineAm");
-  nineAmTxt.textContent = nineAM;
+// handle saving 
+ 
+function saveCalendar(){
+  console.log('help me');
+   nineAm = nineAmTxt.value; 
+  localStorage.setItem("nineAm", nineAm);
 
-  alertButton9am.on("click",function(){
+}
 
-        nineAmTxt.textContent = nineAM;
-        localStorage.setItem("nineAm", nineAM);
+// The init() function fires when the page is loaded 
+
+function renderLastCalendar(){
+ var renderLast9am = localStorage.getItem("nineAm"); 
+ console.log(renderLast9am);
+ document.getElementById("9amTxt").innerHTML = renderLast9am;
+
+  
+}
 
 
-    });
+alertButtonNineAm.addEventListener("click", function(event){
+  event.preventDefault();
+  saveCalendar();
+  renderLastCalendar();
+});
 
+renderLastCalendar();
 
 
 
